c4eefb006d8b69fd0ab172efe901dade1d84ec88
qinx0 <qinx.developer@gmail.com>
2025-12-19 20:14:05 -0300

    nvm pysimplegui died

--- MACRO.py
+++ MACRO.py
@@ -1,23 +1,10 @@
 import pyautogui
-import PySimpleGUI as sg
+import sys
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtUiTools import QUiLoader
 
-screenWidth, screenHeight = pyautogui.size()
-# All the stuff inside your window.
-layout = [  [sg.Text("What's your name?")],
-            [sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
-
-# Create the Window
-window = sg.Window('Hello Example', layout)
-
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-
-    # if user closes window or clicks cancel
-    if event == sg.WIN_CLOSED or event == 'Cancel':
-        break
-
-    print('Hello', values[0], '!')
-
-window.close()
+loader = QUiLoader()
+app = QtWidgets.QApplication(sys.argv)
+window = loader.load("mainwindow.ui", None)
+window.show()
+app.exec()
